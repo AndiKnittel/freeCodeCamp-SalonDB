b4cb4469d42be25fcde1ad9c377acc29f0240e69
AndiKnittel <73914320+AndiKnittel@users.noreply.github.com>
2023-08-21 19:17:21 +0200

    Update salon.sh

--- salon.sh
+++ salon.sh
@@ -1,80 +1,59 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=salon --tuples-only -c"
+PSQL="psql -X --username=freecodecamp --dbname=salon --tuples-only -c"
 
 echo -e "\n~~~~~ MY SALON ~~~~~\n"
 
-echo -e "Welcome to My Salon, how can I help you?\n"
+echo -e "\nWelcome to My Salon, how can I help you?\n"
 
-# Service Menu
-SERVICE_MENU () {
+
+SERVICES () {
 
   if [[ $1 ]] 
   then
     echo $1
   fi
-  echo -e "Welcome to My Salon, here are the available services"
+
   echo -e "How can I help you?\n"
   
-  # get service list 
-  SERVICE_INFO=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
-  echo "$SERVICE_INFO" | sed "s/ |/)/"
+  SERVICE_ID=$($PSQL "SELECT service_id, name FROM services ORDER BY service_id")
+  echo "$SERVICE_ID" | sed "s/ |/)/"
+  read SELECTED_ID
   
-  # get service name. 
-  read SERVICE_ID_SELECTED
-  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id =$SERVICE_ID_SELECTED")
+  SERVICE_NAME=$($PSQL "SELECT name FROM services WHERE service_id =$SELECTED_ID")
 
-  # if not found
   if [[ -z $SERVICE_NAME ]]
   then 
-    SERVICE_MENU "I could not find that service. What would you like today?"
+    SERVICEs "I could not find that service. What would you like today?"
   else 
-    CUSTOMER_INFO_MENU
+    CUSTOMER_MENU
   fi
-
-
-  # send to main menu
 }
 
-# Custumer Info Menu
-CUSTOMER_INFO_MENU () {
-  # get phone number
+CUSTOMER_MENU () {
   echo -e "\nWhat's your phone number?"
-  read CUSTOMER_PHONE
+  read PHONE
   
-  # check customer name
-  CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$CUSTOMER_PHONE'")
+  NAME=$($PSQL "SELECT name FROM customers WHERE phone='$PHONE'")
 
-  # if not found
-  if [[ -z $CUSTOMER_NAME ]]
+  if [[ -z $NAME ]]
   then
-    # get customer name
     echo -e "\nI don't have a record for that phone number. what's your name?"
-    read CUSTOMER_NAME
-
-    # enter into database
-    INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name,phone) VALUES('$CUSTOMER_NAME','$CUSTOMER_PHONE')")
+    read NAME
+    INSERT_CUSTOMER=$($PSQL "INSERT INTO customers (name,phone) VALUES ('$NAME','$PHONE')")
   fi
-  # move for appointment
-  APPOINTMENT_MENU
+
+  APPOINTMENT
   
 }
 
-#Appointment Menu
-APPOINTMENT_MENU () {
-# get customer id
-CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$CUSTOMER_PHONE'")
+APPOINTMENT () {
+CUSTOMER_ID=$($PSQL "SELECT customer_id FROM customers WHERE phone='$PHONE'")
 
-# get service time
-echo -e "\nWhat time would like your $(echo $SERVICE_NAME| sed 's/^ *//'), $CUSTOMER_NAME?"
-read SERVICE_TIME
-
-# enter into appointments
-INSERT_APPOINTMENT_RESULT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID,$SERVICE_ID_SELECTED,'$SERVICE_TIME')")
-
-# greet 
-echo -e "\nI have put you down for a $(echo $SERVICE_NAME| sed 's/^ *//') at $SERVICE_TIME, $CUSTOMER_NAME."
+echo -e "\nWhat time would like your $(echo $SERVICE_NAME| sed 's/^ *//'), $NAME?"
+read APPOINTMENT_TIME
+INSERT_APPOINTMENT=$($PSQL "INSERT INTO appointments(customer_id, service_id, time) VALUES($CUSTOMER_ID, $SERVICE_ID,'$APPOINTMENT_TIME')")
+echo -e "\nI have put you down for a $(echo $SERVICE_NAME| sed 's/^ *//') at $APPOINTMENT_TIME, $NAME."
 
 }
 
-
-SERVICE_MENU
+SERVICES
